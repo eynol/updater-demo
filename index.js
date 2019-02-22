3a59232526806d6295cdd4809546129da85c37fb
eynol <625608852@qq.com>
2019-02-22 17:45:57 +0800

    read version

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 #!/usr/bin/env node
 const { exec } = require('child_process');
-
-exec('git tag', (error, output, stderr) => {
-    if (error) {
-        process.exit(0);
-    } else {
-        console.log(output);
-    }
-})
+require('./main');
--- main.js
+++ main.js
@@ -1,54 +1,63 @@
-const { exec } = require("child_process");
 const fs = require("fs");
 const path = require("path");
+const utils = require("./utils.js");
+const { execPromise } = utils;
+
+const CLI_DIR = __dirname;
+const PROJ_DIR = process.cwd();
+const CACHE_DIR = path.resolve(__dirname, "./.cache");
+const currentVersion = require(path.resolve(PROJ_DIR, ".boilerplate.json"))
+  .version;
+process.argv.splice(0, 2);
+const updateToVersion = process.argv[0];
 
 (async function main() {
   await updateBoilerplate();
-  exec(
-    "git tag",
-    { cwd: path.resolve("./.cache/") },
-    (error, output, stderr) => {
-      if (error) {
-        process.exit(0);
-      } else {
-        console.log(output);
-      }
-    }
-  );
+  let tags = await getTags();
+
+  let pIndex = tags.indexOf(currentVersion);
+  let tIndex = tags.indexOf(updateToVersion);
+  console.log(currentVersion, updateToVersion);
+  console.log(pIndex, tIndex);
+
+  for (let i = pIndex; i <= tIndex; i++) {
+    let version = tags[i];
+    await update(version);
+  }
 })();
 
 async function updateBoilerplate() {
-  return new Promise((resolve, reject) => {
-    if (fs.existsSync(path.resolve("./.cache"))) {
-      exec(
-        "git reset --hard && git pull",
-        { cwd: __dirname },
-        (err, stdout, stderr) => {
-          if (err) {
-            console.log(err);
-            reject("");
-            process.exit(1);
-          } else {
-            console.log(stdout);
-            resolve();
-          }
-        }
-      );
-    } else {
-      exec(
-        "git clone https://github.com/eynol/updater-demo.git ./.cache",
-        { cwd: __dirname },
-        (err, stdout, stderr) => {
-          if (err) {
-            console.log(err);
-            reject("");
-            process.exit(1);
-          } else {
-            console.log(stdout);
-            resolve();
-          }
-        }
-      );
-    }
+  if (fs.existsSync(CACHE_DIR)) {
+    let result = await execPromise(
+      "git reset --hard -q && git checkout master && git pull origin master",
+      {
+        cwd: CACHE_DIR
+      }
+    );
+    console.log(result);
+  } else {
+    let result = await execPromise(
+      "git clone https://github.com/eynol/updater-demo.git ./.cache",
+      { cwd: CLI_DIR }
+    );
+    console.log(result);
+  }
+}
+
+async function getTags() {
+  let output = await execPromise("git tag", { cwd: CACHE_DIR });
+  if (output.length) {
+    let tags = output.trim().split("\n");
+    return utils.sortGitTags(tags);
+  }
+}
+
+async function update(version) {
+  let result = await execPromise(`git checkout ${version} && git ls-files`, {
+    cwd: CACHE_DIR
   });
+
+  // do update 
+  console.log("-----------");
+  console.log(result);
 }
