ae356baa7ba1de1c1588b6ab3ede53d641cf0f11
eynol <625608852@qq.com>
2019-03-01 19:29:49 +0800

    [Auto] Override version

--- u/updateJest.js
+++ u/updateJest.js
@@ -5,6 +5,10 @@ process.argv.splice(0, 2);
 
 let packageJSON = fs.readFileSync(process.argv[0]).toJSON();
 
+if(!packageJSON.jest){
+  packageJSON.jest = {};
+}
+
 Object.assign(packageJSON.jest, {
   "moduleNameMapper": {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/tools/assetsTransformer.js",
